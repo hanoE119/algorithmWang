70a72e698c72d4fab31ccbbc443ff3a8190caa8c
hanoE119 <af90ef@gmail.com>
2024-02-06 21:17:37 +0900

    3 2피자나눠먹기(3) 풀이

---
+++ enter/2피자나눠먹기(3)2.js
@@ -0,0 +1,7 @@
+function solution(slice, n) {
+    var answer = Math.ceil(n/slice);
+    console.log(answer)
+    return answer;
+}
+
+solution(7,10)
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log(5/10)
+
+console.log(Math.floor(7/10))
+
+console.log(7%10)
